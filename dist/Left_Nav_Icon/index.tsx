a93e3eb9e160a76c339116b08f2fca58a8862692
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-07-17 15:00:05 +0600

    dist folder added

---
+++ dist/Left_Nav_Icon/index.tsx
@@ -0,0 +1,21 @@
+import Finance from "./Finance.png";
+import Continuous_Control_Management from "./Continuous Control Management.png";
+import Continuous_Monitoring from "./Continuous monitoring.png";
+import Control_Managment from "./Control Management.png";
+import Issu_Management from "./Issue Management.png";
+import Risk_Managment from "./Risk Management.png";
+import Result_Management from "./Result Management.png";
+import Tools from "./Tools.png";
+import Setup_and_Administration from "./Setup and Administration.png";
+
+export {
+  Finance,
+  Continuous_Control_Management,
+  Continuous_Monitoring,
+  Control_Managment,
+  Issu_Management,
+  Risk_Managment,
+  Result_Management,
+  Tools,
+  Setup_and_Administration,
+};
